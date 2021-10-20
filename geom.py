555aac22dba842eb431d53aa7391286402ffd6f5
pbandierapaiva <paiva@unifesp.br>
2021-10-20 10:04:49 -0300

    20/10/2021 - classe Reta

--- geom.py
+++ geom.py
@@ -26,11 +26,11 @@ class Ponto:
 		return self.__x == ponto.getx() and self.__y == ponto.gety()
 		
 	def setx(self, px):
-		if type(px)!=int:
+		if type(px)!=int and type(px)!=float:
 			raise TypeError
 		self.__x = px
 	def sety(self, py):
-		if type(py)!=int:
+		if type(py)!=int  and type(py)!=float:
 			raise TypeError
 		self.__y = py
 	def getx(self):
@@ -54,6 +54,14 @@ class Ponto:
 		return ( (ponto.getx()-self.__x)**2 + (ponto.gety()-self.__y)**2 )**0.5
 		
 class Reta:
+	"""A classe Reta define uma reta através dos coeficientes da equação y = ax +b.
+	Construtor sobrecarregado que crie uma instância de reta a partir de dois pontos, a partir dos coeficientes (angular e linear);
+	Métodos de acesso para o coeficiente angular e para o coeficiente linear da reta;
+	Um método que gere e retorne a representação String da reta;
+	Um método que verifique se um ponto dado pertence a reta;
+	Retorne a distância entre um ponto e a reta
+	Um método que dada uma outra reta, retorne o ponto de interseção da reta dada ou null se as retas forem paralelas.
+	"""
 	def __init__(self, *args):
 		if len(args)!=2:
 			raise TypeError
@@ -74,7 +82,44 @@ class Reta:
 		return self.__angular
 	def getlin(self):
 		return self.__linear
+	def __contains__(self, ponto):
+#		return ponto.gety() == self.getang() *  ponto.getx() + self.getlin()
+		a = self.getang()     
+		l = self.getlin()
+		x = ponto.getx() 
+		y = ponto.gety()
+		return y == a*x+l
+	def dist(self, ponto):
+		# https://brasilescola.uol.com.br/matematica/distancia-entre-ponto-reta.htm
+#		return (self.getang()*ponto.getx()+self.getlin()+ponto.gety())/(self.getang()*2+1)**0.5)
+		a = self.getang()     
+		l = self.getlin()
+		x = ponto.getx() 
+		y = ponto.gety()
+		d = ( (a * x) + y + l ) / ( (a**2 + 1)**0.5 )
+		return d  				
+	def intercepta(self, r ):
+		# x , y ?
+		#             y = ( a2*l1 - a1*l2 ) / (a2-a1)
+		a1 = self.getang() 	
+		a2 = r.getang()
+		l1 = self.getlin()
+		l2 = r.getlin()
 		
+		if a2==a1:  # paralelas
+			return None
+		y = ( a2*l1 - a1*l2 ) / (a2-a1)		
+		x = (y-l1) / a1
+		x2 = (y-l2) / a2
+		print(x ,  x2)
 		
+		return Ponto(x,y)
+
+
+
+
+
+
+
 
 
